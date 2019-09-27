7ed55e0fa1a307a447c3e228948b63a6ad5ac924
Andrew Artajos <andrew.artajos@icloud.com>
2019-09-27 21:55:04 +1000

    review(aartajos): testing listings

---
+++ Lesson16/review/workbench/src/async.test.js
@@ -0,0 +1,16 @@
+test("Failing async call", () => {
+  expect.hasAssertions();
+  setTimeout(() => {
+    expect(true).toBeTruthy();
+  }, 1000);
+});
+
+test("Passing async call", () => {
+  expect.hasAssertions();
+  function later(delay) {
+    return new Promise(function(resolve) {
+      setTimeout(resolve, delay);
+    });
+  }
+  return later(1000).then(() => expect(true).toBeTruthy());
+});
