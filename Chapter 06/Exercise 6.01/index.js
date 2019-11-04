b10393cb19297434c5dd61af0e2bf55384a5ceb5
Lee Sylvester <lee.sylvester@gmail.com>
2019-11-04 08:11:28 +0000

    Update exercise 6.01

--- Chapter 06/Exercise 6.01/index.js
+++ Chapter 06/Exercise 6.01/index.js
@@ -1,13 +1,14 @@
 var curTime, lastTime,
   interval = 500;
 function main() {  // primary function
-  let i = 100;  // decrementing counter
-  while (i-- > 0) {  // enter loop
+  let running = true;  // loop running flag
+  while (running) {  // enter loop
     curTime = new Date().getTime();
     lastTime = lastTime || curTime;
     if (curTime - lastTime > interval) {
       console.log(curTime);
       lastTime = curTime;
+      running = false;
     }
   }
 }
